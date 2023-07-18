acbd6b6714190a8c41f3c6c48c8d771d6099b76c
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2023-07-18 18:19:25 +0200

    Authorization implemented for pec service

---
+++ pec-service/src/main/java/com/gamma/pecservice/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.gamma.pecservice.security;
+
+import com.gamma.auth.AuthorizationFilter;
+import com.gamma.auth.Sha256PasswordEncoder;
+import com.gamma.auth.UserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Value("${identity.jwt.prefix}")
+    private String prefix;
+    @Value("${identity.jwt.secret}")
+    private String secret;
+    @Value("${identity.jwt.param}")
+    private String param;
+
+    @Autowired
+    private UserDetailService userDetailService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new AuthorizationFilter(this.authenticationManager(), this.param, this.prefix, this.secret), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailService)
+                .passwordEncoder(new Sha256PasswordEncoder());
+    }
+}
